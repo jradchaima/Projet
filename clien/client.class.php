3b7777f2130d36d1592c39ac9d8ac329375284bd
Ameni121 <salhiamani20@gmail.com>
2019-12-05 16:51:27 +0100

    registre

---
+++ clien/client.class.php
@@ -0,0 +1,93 @@
+<?php
+
+    require 'dbconnect.class.php';
+
+    class Client
+    {
+        private $cnx;
+
+        public function __construct()
+        {
+            $db = new BasesDonnees;
+            $this->cnx = $db->connectDB();
+        }
+
+        public function readAllClients()
+        {
+            try {
+                $req = 'SELECT * FROM clients';
+                $result = $this->cnx->prepare($req);
+                $result->execute();
+                return $result;
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+        }
+
+        public function showOneClient($id)
+        {
+            try {
+                $req = 'SELECT * FROM clients WHERE id= :clt_id';
+                $result = $this->cnx->prepare($req);
+                $result->bindParam(':clt_id', $id);
+                $result->execute();
+                return $result;
+            } catch (PDOException $e) {
+                echo $e->getMessage();
+            }
+        }
+
+        public function addNewClient($name,$email,$phone,$adresse,$pwd)
+        {
+            try {
+                $sql = "INSERT INTO client(nom,email,pwd,phone,adresse) VALUES (:clt_nom,:clt_email,:clt_pwd,:clt_tel,:clt_adr)";
+            $result = $this->cnx->prepare($sql);
+            $result->bindparam(":clt_nom", $name);
+            $result->bindparam(":clt_email", $email);
+            $result->bindparam(":clt_pwd", $pwd);
+            $result->bindparam(":clt_adr", $adresse);
+            $result->bindparam(":clt_tel", $phone);
+            $result->execute();
+            return $result;
+            } catch (PDOException $ex) {
+                echo $ex->getMessage();
+            }
+        }
+
+        public function updateClient($id, $nom, $prenom, $dateBirth, $adr, $tel)
+        {
+            try {
+                $sql = 'UPDATE clients
+                        SET nom = :clt_nom,
+                            prenom = :clt_prenom,
+                            datenaissance = :clt_dateN,
+                            adresse = :clt_adr,
+                            tel = :clt_tel
+                        WHERE id = :clt_id';
+                $result = $this->cnx->prepare($sql);
+                $result->bindparam(":clt_id", $id);
+                $result->bindparam(":clt_nom", $nom);
+                $result->bindparam(":clt_prenom", $prenom);
+                $result->bindparam(":clt_dateN", $dateBirth);
+                $result->bindparam(":clt_adr", $adr);
+                $result->bindparam(":clt_tel", $tel);
+                $result->execute();
+                return $result;
+
+            } catch (PDOException $exception) {
+                echo $exception->getMessage();
+            }
+        }
+        public function deleteClient($id)
+        {
+            try {
+                $sql = 'DELETE FROM clients WHERE id = :clt_id';
+                $result = $this->cnx->prepare($sql);
+                $result->bindparam(":clt_id", $id);
+                $result->execute();
+                return $result;
+            } catch (PDOException $exception) {
+                echo $exception->getMessage();
+            }
+        }
+    }
---
+++ clien/create.php
@@ -0,0 +1,17 @@
+
+    <?php
+
+        $name=$_POST['name'];
+        $email=$_POST['email'];
+        $phone=$_POST['phone'];
+        $adresse=$_POST['adresse'];
+        $pwd=$_POST['pwd'];
+
+
+        if (!empty($_POST)) {
+            include 'client.class.php';
+            $client = new Client;
+            $rep = $client->addNewClient($name,$email,$phone,$adresse,$pwd);
+           header('location:index.php');          
+        }
+    ?>
